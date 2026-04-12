47fb21e2507eb42ce429d3d2cb7b9bd82cf317da
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-04-11 20:44:29 -0600

    Add project files.

---
+++ src/PvZRMod/Core/Core.cs
@@ -0,0 +1,11 @@
+﻿namespace PvZRMod;
+
+using MelonLoader;
+
+public sealed class Core : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        this.LoggerInstance.Msg("Hello world!");
+    }
+}
---
+++ src/PvZRMod/Core/Metadata/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using MelonLoader;
+
+using PvZRMod;
+using PvZRMod.Metadata;
+
+[assembly: MelonInfo(typeof(Core), ModInfo.Name, ModInfo.Version, ModInfo.Author, ModInfo.DownloadLink)]
+[assembly: MelonGame("PopCap Games", "PvZ Replanted")]
