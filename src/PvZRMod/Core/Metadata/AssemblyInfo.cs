e4e467308891feaed6ee981315e583bd3bb79a22
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-07-20 04:18:38 -0600

    refactor: use PvZR.Sdk

--- src/PvZRMod/Core/Metadata/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using MelonLoader;
-
-using PvZRMod;
-using PvZRMod.Metadata;
-
-[assembly: MelonInfo(typeof(Core), ModInfo.Name, ModInfo.Version, ModInfo.Author, ModInfo.DownloadLink)]
-[assembly: MelonGame("PopCap Games", "PvZ Replanted")]
